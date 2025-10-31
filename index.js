b4aea48d3484d97f1d1a7748a33d71c7f3e74d1a
Sufyan Ali <sufyanpakpatttan@mail.com>
2025-10-31 08:06:21 +0500

    233

--- index.js
+++ index.js
@@ -26,11 +26,13 @@ app.use("/api/user-agents", agentRoutes);
 const authRoutes = require("./routes/AuthRoutes");
 app.use("/api/auth", authRoutes);
 
+
 // =========================
 // 🧠 MongoDB Connection
 // =========================
 const PORT = process.env.PORT || 5000;
 const MONGO_URI = process.env.MONGO_URI || "mongodb://127.0.0.1:27017/listingsDB";
+
 
 mongoose
   .connect(MONGO_URI) // ⚡ Deprecated options removed
